722897411c3b1fe6161bf2679ecb020e76b454ed
may_ <lammayyin@gmail.com>
2024-07-14 23:51:39 +0200

    Implemented Mongoose to connect endpoints to MongoDB
    Added schema in models.js and updated all endpoints to work using Mongoose

--- Express/index.js
+++ Express/index.js
@@ -2,21 +2,26 @@ const express = require('express'),
   bodyParser = require('body-parser'),
   uuid = require('uuid');
 
-//mongoose related
+
+const morgan = require ("morgan");
+const app = express();
+//mongoose related:
 const mongoose = require('mongoose');
 const Models = require('./models.js');
-  
+console.log(Models);   
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+mongoose.connect('mongodb://localhost:27017/cfDB')
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB', err));
 //cfDB - name of database when created with powerShell
 
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const cors = require('cors'); 
-
-const app = express();
 
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -27,478 +32,175 @@ app.use(bodyParser.json());
 // Enable CORS for all routes
 app.use(cors());
 
-let users = [
-  { id: 1,
-    name: "Max Musterman",
-    favouriteMovies: ["The Godfather"]    
-  },
-  { id: 2,
-    name: "Jane Doe",
-    favouriteMovies: ["Pulp Fiction"]    
-  }
-
-]
-
-
-
-let movies = [
-  {
-    id: 1,
-    title: "The Godfather",
-    year: 1972,
-    description: "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
-    genre: {
-      name: "Crime",
-      description: "Crime films center on the lives of criminals."
-    },
-    director: {
-      name: "Francis Ford Coppola",
-      bio: "Francis Ford Coppola is an American film director, producer, and screenwriter known for his epic film The Godfather and its sequels. He has won multiple Academy Awards.",
-      birthday: "April 7, 1939"
-    },
-    imageURL: "https://m.media-amazon.com/images/M/MV5BNzk3YzkyMjYtZTYyOC00Y2ZkLWExMWItNWZjMzlhNWNjYzYxXkEyXkFqcGdeQXVyMTEwNDU1MzEy._V1_FMjpg_UX800_.jpg",
-    featured: true
-  },
-  {
-    id: 2,
-    title: "Star Wars: Episode IV – A New Hope",
-    year: 1977,
-    description: "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee, and two droids to save the galaxy from the Empire's world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader.",
-    genre: {
-      name: "Adventure",
-      description: "Adventure films involve exploration or quests."
-    },
-    director: {
-      name: "George Lucas",
-      bio: "George Lucas is an American film director, producer, and screenwriter best known for creating the Star Wars and Indiana Jones franchises. He is a pioneer in modern filmmaking techniques.",
-      birthday: "May 14, 1944"
-    },
-    imageURL: "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
-    featured: true
-  },
-  {
-    id: 3,
-    title: "Pulp Fiction",
-    year: 1994,
-    description: "The lives of two mob hitmen, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
-    genre: {
-      name: "Crime",
-      description: "Crime films center on the lives of criminals."
-    },
-    director: {
-      name: "Quentin Tarantino",
-      bio: "Quentin Tarantino is an American filmmaker and screenwriter known for his stylized, non-linear storytelling and his love of film history. He has directed several critically acclaimed movies.",
-      birthday: "March 27, 1963"
-    },
-    imageURL: "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY1200_CR97,0,630,1200_AL_.jpg",
-    featured: false
-  }
-];
 
 require('./swagger')(app);
 
-/**
- * @swagger
- * /movies:
- *   get:
- *     summary: Retrieve a list of movies
- *     responses:
- *       200:
- *         description: A list of movies
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 type: object
- */
-app.get('/movies', (req, res) => {
-  res.status(200).json(movies);
+//default text response when at /
+app.get("/", (req, res) =>{
+  res.send("Welcome to MyFlix");
 });
 
-/**
- * @swagger
- * /users:
- *   get:
- *     summary: Retrieve a list of users
- *     responses:
- *       200:
- *         description: A list of users
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 type: object
- */
-app.get('/users', (req, res) => {
-  res.status(200).json(users);
-});
+//Get all Movies
+app.get("/movies", (req,res) =>{
+  Movies.find()
+  .then((movies) => {
+    res.status(201).json(movies);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
+})
 
-/**
- * @swagger
- * /movies/{title}:
- *   get:
- *     summary: Retrieve a movie by title
- *     parameters:
- *       - in: path
- *         name: title
- *         required: true
- *         schema:
- *           type: string
- *         description: The movie title
- *     responses:
- *       200:
- *         description: A movie object
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *       400:
- *         description: No such movie
- */
-app.get('/movies/:title', (req, res) => {
-  const { title } = req.params; 
-  const movie = movies.find(movie => movie.title === title);
-
-  if (movie){
-    res.status(200).json(movie);
-  } else {
-    res.status(400).send('no such movie');
-  }
-});
-
-/**
- * @swagger
- * /movies/genre/{genreName}:
- *   get:
- *     summary: Retrieve movies by genre
- *     parameters:
- *       - in: path
- *         name: genreName
- *         required: true
- *         schema:
- *           type: string
- *         description: The genre name
- *     responses:
- *       200:
- *         description: A genre object
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *       400:
- *         description: No such genre
- */
-app.get('/movies/genre/:genreName', (req, res) => {
-  const { genreName } = req.params;
-  const movie = movies.find(movie => movie.genre.name.toLowerCase() === genreName.toLowerCase());
-
-  if (movie) {
-    res.status(200).json(movie.genre);
-  } else {
-    res.status(404).send('No such genre');
-  }
-});
-
-/**
- * @swagger
- * /movies/director/{directorName}:
- *   get:
- *     summary: Retrieve movies by director
- *     parameters:
- *       - in: path
- *         name: directorName
- *         required: true
- *         schema:
- *           type: string
- *         description: The director name
- *     responses:
- *       200:
- *         description: A director object
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *       400:
- *         description: No such director
- */
-app.get('/movies/director/:directorName', (req, res) => {
-  const { directorName } = req.params; 
-  const movie = movies.find(movie => movie.director.name.toLowerCase() === directorName.toLowerCase());
-
-  if (movie) {
-    const director = movie.director;
-    res.status(200).json(director);
-  } else {
-    res.status(400).send('No such director');
-  }
-});
-
-/**
- * @swagger
- * /users:
- *   post:
- *     summary: Add a new user
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               name:
- *                 type: string
- *     responses:
- *       201:
- *         description: The created user object
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *       400:
- *         description: Users need names
- */
-// app.post('/users', (req, res) => {
-//   const newUser = req.body;
-
-//   if (newUser.name){
-//     newUser.id = uuid.v4();
-//     users.push(newUser);
-//     res.status(201).json(newUser);
-//   } else {
-//     res.status(400).send('users need names');
-//   }
-// });
-
-//Add a user
-/* We’ll expect JSON in this format
-{
-  ID: Integer,
-  Username: String,
-  Password: String,
-  Email: String,
-  Birthday: Date
-}*/
-app.post('/users', async (req, res) => {
-  await Users.findOne({ Username: req.body.Username })
-    .then((user) => {
-      if (user) {
-        return res.status(400).send(req.body.Username + 'already exists');
-      } else {
-        Users
-          .create({
-            Username: req.body.Username,
-            Password: req.body.Password,
-            Email: req.body.Email,
-            Birthday: req.body.Birthday
-          })
-          .then((user) =>{res.status(201).json(user) })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).send('Error: ' + error);
-        })
-      }
+// Get all users
+app.get('/users', async (req, res) => {
+  await Users.find()
+    .then((users) => {
+      res.status(201).json(users);
     })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send('Error: ' + error);
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
     });
 });
 
 
-/**
- * @swagger
- * /users/{id}:
- *   put:
- *     summary: Update user info
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *         description: The user ID
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               name:
- *                 type: string
- *     responses:
- *       200:
- *         description: The updated user object
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *       400:
- *         description: No such user
- */
-app.put('/users/:id', (req, res) => {
-  const {id} = req.params;
-  const updatedUser = req.body;
 
-  let user = users.find(user => user.id === parseInt(id, 10));
+//get move by title
+app.get('/movies/:title', (req, res) => {
+  Movies.findOne({title: req.params.title})
+    .then((movie) =>{
+      res.json(movie);
+    })
+    .catch((err) =>{
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
-  if (user) {
-    user.name = updatedUser.name;
-    user.favouriteMovies = updatedUser.favouriteMovies; // Update other properties as needed
-    res.status(200).json(user);
-  } else {
-    res.status(404).send('No such user');
+
+
+//get movie details by genrename
+app.get('/movies/genre/:name', (req, res) => {
+  Movies.findOne({ 'genre.name': req.params.name })
+    .then((genre) => {
+      res.json(genre);
+  })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error ' + err); 
+      });
+});
+
+//get info by director name
+app.get('/director/:name', (req, res) => {
+  Movies.findOne({ 'directors.name': req.params.name })
+    .then((director) => {
+      res.json(director);
+  })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error ' + err); 
+      });
+});
+
+
+//create new user
+app.post('/users', async (req, res) => {
+  try {
+    const existingUser = await Users.findOne({ username: req.body.username });
+    
+    if (existingUser) {
+      return res.status(400).send(req.body.username + ' already exists');
+    }
+    
+    const newUser = await Users.create({
+      name: req.body.name,
+      username: req.body.username,
+      password: req.body.password,
+      email: req.body.email,
+      birthday: req.body.birthday,
+      favorite_movies: req.body.favorite_movies
+    });
+    
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Error: ' + error);
   }
 });
 
-/**
- * @swagger
- * /users/{id}/{movietitle}:
- *   post:
- *     summary: Add favourite movie for user
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *         description: The user ID
- *       - in: path
- *         name: movietitle
- *         required: true
- *         schema:
- *           type: string
- *         description: The movie title
- *     responses:
- *       200:
- *         description: Movie added to user's favourites
- *       400:
- *         description: No such user
- */
-app.post('/users/:id/:movietitle', (req, res) => {
-  const { id, movietitle } = req.params;
 
-  let user = users.find(user => user.id == id);
+//update user details
+app.put('/users/:username', async (req, res) => {
+  await Users.findOneAndUpdate({ username: req.params.username }, { $set:
+    {
+      name: req.body.name,
+      username: req.body.username,
+      password: req.body.password,
+      email: req.body.email,
+      birthday: req.body.birthday,
+      favorite_movies: req.body.favorite_movies
+    }
+  },
+  { new: true }) // This line makes sure that the updated document is returned
+  .then((updatedUser) => {
+    res.json(updatedUser);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  })
 
-  if (user){
-    user.favouriteMovies.push(movietitle);
-    res.status(200).send(`${movietitle} has been added to user ${id}'s array`); 
-  } else {
-    res.status(400).send('no such user');
-  }
 });
 
-/**
- * @swagger
- * /users/{id}/{movietitle}:
- *   delete:
- *     summary: Remove favourite movie for user
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *         description: The user ID
- *       - in: path
- *         name: movietitle
- *         required: true
- *         schema:
- *           type: string
- *         description: The movie title
- *     responses:
- *       200:
- *         description: Movie removed from user's favourites
- *       400:
- *         description: No such user
- */
-app.delete('/users/:id/:movietitle', (req, res) => {
-  const { id, movietitle } = req.params;
-
-  let user = users.find(user => user.id == id);
-
-  if (user){
-    user.favouriteMovies = user.favouriteMovies.filter(title => title !== movietitle);
-    res.status(200).send(`${movietitle} has been removed from user ${id}'s array`); 
-  } else {
-    res.status(400).send('no such user');
-  }
+// Delete a user by username
+app.delete('/users/:username', async (req, res) => {
+  await Users.findOneAndDelete({ username: req.params.username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.username + ' was not found');
+      } else {
+        res.status(200).send(req.params.username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-/**
- * @swagger
- * /users/{id}:
- *   delete:
- *     summary: Delete a user
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *         description: The user ID
- *     responses:
- *       200:
- *         description: The updated list of users
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 type: object
- *       400:
- *         description: No such user
- */
-app.delete('/users/:id', (req, res) => {
-  const { id } = req.params;
 
-  let user = users.find(user => user.id == id);
 
-  if (user) {
-    users = users.filter(user => user.id != id);
-    res.json(users);
-  } else {
-    res.status(400).send('no such user');
-  }
+
+// Add a movie to a user's list of favorites
+app.post('/users/:username/movies/:MovieID', async (req, res) => {
+  await Users.findOneAndUpdate({ username: req.params.username }, {
+     $push: { favorite_movies: req.params.MovieID }
+   },
+   { new: true }) // This line makes sure that the updated document is returned
+  .then((updatedUser) => {
+    res.json(updatedUser);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
 });
 
-/**
- * @swagger
- * /movies:
- *   post:
- *     summary: Add a new movie
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               title:
- *                 type: string
- *     responses:
- *       201:
- *         description: The created movie object
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *       400:
- *         description: Missing title in request body
- */
-app.post('/movies', (req, res) => {
-  let newMovie = req.body;
-
-  if (!newMovie.title) {
-    const message = 'Missing title in request body';
-    res.status(400).send(message);
-  } else {
-    newMovie.id = uuid.v4();
-    movies.push(newMovie);
-    res.status(201).send(newMovie);
-  }
+// Delete a movie to a user's list of favorites
+app.delete('/users/:username/movies/:MovieID', async (req, res) => {
+  await Users.findOneAndUpdate({ username: req.params.username }, {
+     $pull: { favorite_movies: req.params.MovieID }
+   },
+   { new: true }) // This line makes sure that the updated document is returned
+  .then((updatedUser) => {
+    res.json(updatedUser);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
 });
 
 
---
+++ Express/models.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+ //notes- required means: field is required for all documents that follow this schema, and it ensures that the key, as well as the type of data it stores as its value, will be uniform for all documents in the collection.
+//notes- genre is key for movieSchema, and is a subdocument containing details like name and description
+//[] - array data type
+
+
+
+
+let movieSchema = mongoose.Schema({
+
+    title: {type: String, required: true}, 
+    description: {type: String, required: true},
+    
+    genre: {
+      name: String,
+      description: String
+    },
+    directors: {
+      name: String,
+      bio: String
+    },
+    actors: [String],
+    imagepath: String,
+    featured: Boolean
+  });
+  
+  let userSchema = mongoose.Schema({
+    name: {type: String, required: true},
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+    email: {type: String, required: true},
+    birthday: Date,
+    favorite_movies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }] 
+  });
+      //notes-  the reference method is being used to link “Users” collection with“Movies” collection
+    //alternatively can use .populate() to contain an array of actual movie documents. you can write a single query to pull information about the user along with their favorite movies compared to reference by using embedded data
+  
+  //This will create collections called “db.movies” and “db.users":
+  //Keep an eye on your capital letters and plurals here—any titles you pass through will come out on the other side as lowercase and pluralized. For instance, above, the titles Movie and User will create collections called “db.movies” and “db.users”, respectively. eg Book - collection will be called "db.books"
+  let Movie = mongoose.model('Movie', movieSchema);
+  let User = mongoose.model('User', userSchema);
+
+  
+  //export these so they can be imported in index.js so the API endpoints can use these to query MongoDB database according to the defined schema
+  module.exports.Movie = Movie;
+  module.exports.User = User;
+  
